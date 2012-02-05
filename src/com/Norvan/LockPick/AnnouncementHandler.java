b923a830d780787ab6d834de4739bebb305539d6
ngorgi <nourvan@gmail.com>
2012-02-05 13:06:42 -0800

    renamed package

---
+++ src/com/Norvan/LockPick/AnnouncementHandler.java
@@ -0,0 +1,110 @@
+package com.Norvan.LockPick;
+
+import android.content.Context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ngorgi
+ * Date: 2/5/12
+ * Time: 2:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnnouncementHandler {
+    int userType;
+    VibrationHandler vibrationHandler;
+    TTSHandler tts;
+        Context context;
+    public AnnouncementHandler(Context context, VibrationHandler vibrationHandler) {
+        userType = SharedPreferencesHandler.getUserType(context);
+        this.vibrationHandler = vibrationHandler;
+        this.context = context;
+        tts = new TTSHandler(context);
+    }
+
+    public void shutDown() {
+        tts.shutDownTTS();
+    }
+
+    public void cancelAnnouncement() {
+        tts.shutUp();
+        vibrationHandler.stopVibrate();
+
+    }
+
+    public void newLaunch() {
+        if (userType == SharedPreferencesHandler.USER_BLIND) {
+            //tts
+            tts.speakPhrase("Turn the phone on its side with the screen facing left. Press the volume button to begin.");
+        } else if (userType == SharedPreferencesHandler.USER_DEAFBLIND) {
+            //morse
+            vibrationHandler.stopVibrate();
+           vibrationHandler.playString("Turn phone on side screen facing left. Press volume to begin.");
+        }
+
+    }
+
+    public void levelStart(int level, int picksLeft) {
+        if (userType == SharedPreferencesHandler.USER_BLIND) {
+            //tts
+            tts.speakPhrase("Level " + String.valueOf(level + 1) + ", " + String.valueOf(picksLeft) + " picks left.");
+        } else if (userType == SharedPreferencesHandler.USER_DEAFBLIND) {
+            //morse
+        }
+    }
+
+    public void levelWon(float time, int wonLevel) {
+        vibrationHandler.playHappy();
+        if (userType == SharedPreferencesHandler.USER_BLIND) {
+            //tts
+            tts.speakPhrase("You beat level " + String.valueOf(wonLevel + 1) + " in " + getTimeString(time) + ". Press volume to continue.");
+        } else if (userType == SharedPreferencesHandler.USER_DEAFBLIND) {
+            //morse
+            vibrationHandler.playString("You won level " + String.valueOf(wonLevel) + " in " + getTimeString(time));
+        }
+        else{
+            tts.speakPhrase("Good Job!");
+        }
+    }
+
+    public void levelLost(int level, int picksLeft) {
+        vibrationHandler.playSad();
+
+        if (userType == SharedPreferencesHandler.USER_BLIND) {
+            //tts
+            tts.speakPhrase("You lost. Press volume to try again");
+        } else if (userType == SharedPreferencesHandler.USER_DEAFBLIND) {
+            //morse
+            vibrationHandler.playString("You lost " + String.valueOf(picksLeft) + " picks left");
+        }
+        else{
+            tts.speakPhrase("You failed");
+        }
+    }
+
+    public void gameOver(int maxLevel) {
+        vibrationHandler.playSad();
+
+        if (userType == SharedPreferencesHandler.USER_BLIND) {
+            //tts
+            if (SharedPreferencesHandler.getHighScore(context) < maxLevel) {
+                tts.speakPhrase("Game Over. Your reached level " + String.valueOf((maxLevel + 1)) + ". A new high score! Press volume for a new game.");
+            }   else{
+                tts.speakPhrase("Game Over. Your reached level " + String.valueOf((maxLevel + 1)) + ". Press volume for a new game.");
+            }
+
+        } else if (userType == SharedPreferencesHandler.USER_DEAFBLIND) {
+            //morse
+            vibrationHandler.playString("Game over. Reached level " + String.valueOf(maxLevel+1));
+        }
+        else{
+            tts.speakPhrase("Game Over");
+        }
+
+    }
+
+    private String getTimeString(float time) {
+        int seconds = (int) time / 1000;
+        return String.valueOf(seconds) + " seconds";
+
+    }
+}
---
+++ src/com/Norvan/LockPick/GameHandler.java
@@ -0,0 +1,184 @@
+package com.Norvan.LockPick;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ngorgi
+ * Date: 2/5/12
+ * Time: 12:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameHandler {
+    int numberOfPicksLeft = 5;
+    int currentLevel = 0;
+    VibrationHandler vibrationHandler;
+    SensorHandler sensorHandler;
+    LevelHandler levelHandler;
+    int lastPressedPosition = -1;
+    boolean keyPressed = false;
+    boolean isPolling = false;
+    boolean gameInProgress = false;
+    int keyPressedPosition = -1;
+    private Handler mHandler = new Handler();
+    GameStatusInterface gameStatusInterface;
+    Context context;
+    int angularVelocityMinimumThreshold = 10;
+    int gameState = 0;
+    public static final int STATE_FRESHLOAD = 0;
+    public static final int STATE_INGAME = 1;
+    public static final int STATE_MIDLEVEL = 2;
+    public static final int STATE_GAMEOVER = 3;
+
+    public GameHandler(Context context, GameStatusInterface gameStatusInterface, VibrationHandler vibrationHandler) {
+        this.context = context;
+        this.gameStatusInterface = gameStatusInterface;
+        this.vibrationHandler = vibrationHandler;
+        levelHandler = new LevelHandler(0);
+        sensorHandler = new SensorHandler(context, sensorHandlerInterface);
+        currentLevel = 0;
+
+        PackageManager paM = context.getPackageManager();
+        if (paM.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE)) {
+            angularVelocityMinimumThreshold = 10;
+        } else {
+            angularVelocityMinimumThreshold = 1000;
+        }
+    }
+
+    public void setSensorPollingState(boolean state) {
+        try {
+            if (state) {
+                sensorHandler.startPolling();
+            } else {
+                sensorHandler.stopPolling();
+            }
+            isPolling = state;
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    public int getGameState() {
+        return gameState;
+    }
+
+
+    public void playCurrentLevel() {
+
+
+        levelHandler = new LevelHandler(currentLevel);
+        gameInProgress = true;
+        keyPressed = false;
+        gameStatusInterface.levelStart(currentLevel, numberOfPicksLeft);
+        if (!isPolling) {
+            sensorHandler.startPolling();
+        }
+        gameState = STATE_INGAME;
+    }
+
+
+    public void gotKeyDown() {
+        keyPressed = true;
+        keyPressedPosition = lastPressedPosition;
+        levelHandler.keyDown(keyPressedPosition);
+        vibrationHandler.stopVibrate();
+    }
+
+    public void gotKeyUp() {
+        keyPressed = false;
+
+    }
+
+    SensorHandler.SensorHandlerInterface sensorHandlerInterface = new SensorHandler.SensorHandlerInterface() {
+        @Override
+        public void newValues(float angularVelocity, int tilt) {
+            if (gameState == STATE_INGAME) {
+                if (keyPressed) {
+                    switch (levelHandler.getUnlockedState(tilt)) {
+                        case -1://Pick Broken
+                            gameState = STATE_MIDLEVEL;
+                            levelLost();
+                            break;
+                        case 0: //In Progress
+                            lastPressedPosition = tilt;
+                            int intensity = levelHandler.getIntensityForPositionWhileUnlocking(tilt);
+                            if ((angularVelocity * 100) > angularVelocityMinimumThreshold) {
+                                if (intensity == -1) {
+                                    vibrationHandler.stopVibrate();
+                                } else {
+                                    vibrationHandler.pulsePWM(intensity);
+                                }
+                            } else {
+                                vibrationHandler.stopVibrate();
+                            }
+                            break;
+                        case 1: //A WINRAR IS YOU!!!
+
+                            levelWon();
+                            gameState = STATE_MIDLEVEL;
+                            break;
+                    }
+                } else {
+                    lastPressedPosition = tilt;
+                    int intensity = levelHandler.getIntensityForPosition(tilt);
+                    if ((angularVelocity * 100) > angularVelocityMinimumThreshold) {
+                        if (intensity == -1) {
+                            vibrationHandler.stopVibrate();
+                        } else {
+                            vibrationHandler.pulsePWM(intensity);
+                        }
+                    } else {
+                        vibrationHandler.stopVibrate();
+                    }
+                }
+            }
+
+
+        }
+    };
+
+    private void levelLost() {
+        vibrationHandler.stopVibrate();
+        gameState = STATE_MIDLEVEL;
+        if (numberOfPicksLeft == 0) {
+            gameState = STATE_GAMEOVER;
+
+            gameStatusInterface.gameOver(currentLevel);
+            if (SharedPreferencesHandler.getHighScore(context) < currentLevel) {
+                SharedPreferencesHandler.setHighScore(context, currentLevel);
+            }
+            currentLevel = 0;
+            numberOfPicksLeft = 5;
+        } else {
+            numberOfPicksLeft--;
+            gameStatusInterface.levelLost(currentLevel, numberOfPicksLeft);
+        }
+    }
+
+    private void levelWon() {
+        gameState = STATE_MIDLEVEL;
+        vibrationHandler.stopVibrate();
+        gameStatusInterface.levelWon(currentLevel, numberOfPicksLeft);
+        currentLevel++;
+
+    }
+
+    public interface GameStatusInterface {
+        public void levelStart(int level, int picksLeft);
+
+        public void levelWon(int newLevel, int picksLeft);
+
+        public void levelLost(int level, int picksLeft);
+
+        public void gameOver(int maxLevel);
+    }
+
+
+}
