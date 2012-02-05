03a53245b07415dc09d004ccb4597108d575ddfc
ngorgi <nourvan@gmail.com>
2012-02-04 16:05:36 -0800

    initial

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -4,25 +4,36 @@ import android.app.Activity;
 import android.content.Context;
 import android.hardware.*;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 
 import java.util.List;
 
-public class MyActivity extends Activity 
-    
+public class MyActivity extends Activity
+
 {
     SensorHandler sensorHandler;
-    /** Called when the activity is first created. */
+    Vibrator vibrator;
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        sensorHandler = new SensorHandler(this);
-     }
+        sensorHandler = new SensorHandler(this, sensorHandlerInterface);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
 
-    
+    SensorHandler.SensorHandlerInterface sensorHandlerInterface = new SensorHandler.SensorHandlerInterface() {
+        @Override
+        public void newValues(int angularVelocity, int tilt) {
+            Log.i("AMP", "angvel "+ String.valueOf(angularVelocity)+ " tilt "+String.valueOf(tilt));
+            if(tilt > 500 && tilt < 600){
+               vibrator.vibrate(100);
+            }
+        }
+    };
 
-   
 }
 
--- src/com/example/SensorHandler.java
+++ src/com/example/SensorHandler.java
@@ -5,7 +5,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
+import java.security.PublicKey;
 import java.util.List;
 
 /**
@@ -17,22 +19,58 @@ import java.util.List;
  */
 public class SensorHandler {
     Context context;
+    int lastTiltReading = -1, lastAngularVelocity = -1;
+    SensorHandlerInterface sensorHandlerInterface;
+    long lastTimestampGyro = 0;
+    long lastTimestampAccel = 0;
 
-    public SensorHandler(Context context) {
+    public SensorHandler(Context context, SensorHandlerInterface sensorHandlerInterface) {
         this.context = context;
+        this.sensorHandlerInterface = sensorHandlerInterface;
         SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(
-                Sensor.TYPE_ORIENTATION);
+                Sensor.TYPE_ACCELEROMETER);
 
         Sensor sensor = sensors.get(0);
         sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_GAME);
+        List<Sensor> sensors2 = sensorManager.getSensorList(
+                Sensor.TYPE_GYROSCOPE);
+
+        Sensor sensor2 = sensors2.get(0);
+        sensorManager.registerListener(sensorEventListener, sensor2, SensorManager.SENSOR_DELAY_GAME);
+
     }
 
+
+    public int getTilt() {
+        return lastTiltReading;
+    }
+
+    public int getAngularVelocity() {
+        return lastAngularVelocity;
+    }
 
     SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
+            long timestamp = sensorEvent.timestamp;
 
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                if(timestamp-lastTimestampAccel > 500000000){
+                    lastTiltReading = (int) (50 * sensorEvent.values[2]) + 500;
+                    lastTimestampAccel = timestamp;
+                }
+            } else if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                if(timestamp-lastTimestampGyro > 500000000){
+                    lastAngularVelocity = (int) sensorEvent.values[2];
+                    sensorHandlerInterface.newValues(lastAngularVelocity, lastTiltReading);
+                    lastTimestampGyro = timestamp;
+                }
+
+
+
+
+            }
 
         }
 
@@ -41,6 +79,10 @@ public class SensorHandler {
             //To change body of implemented methods use File | Settings | File Templates.
         }
     };
+
+    public interface SensorHandlerInterface {
+        public void newValues(int angularVelocity, int tilt);
+    }
 
 
 }
